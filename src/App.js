8451247695ab042c9ad8ee91658c1258b0bb03d4
daccatime <dawsonmcnally@outlook.com>
2021-02-18 18:18:06 +0000

    layout for app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 
 import React, {Component} from 'react';
 import WelcomeMessage from './WelcomeMessage' 
+import './app/App.css';
+import styled from 'styled-components'
 
-
+const MyButton = styled.h1`
+font-size: 1.5em;
+text-align: center;
+color: green;
+`;
 
 class App extends Component {
   render() {
     return (
-    <WelcomeMessage/>
+    <div>
+      <WelcomeMessage/>
+      
+  <MyButton>Hello style</MyButton>  
+    </div>
   );
     }
 }
--- src/WelcomeMessage.js
+++ src/WelcomeMessage.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
 export default function Welcome(){
-    return <h1>Welcome to crypto dash1</h1>;
+    return <h1>Welcome to crypto dash
+        
+    </h1>;
 }
