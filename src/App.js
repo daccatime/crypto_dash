7c4ce35c0c7df8decb34c8232eac2526a87ab271
daccatime <dawsonmcnally@outlook.com>
2021-02-18 12:43:48 +0000

    2nd commit for crypo_dash project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import React, {Component} from 'react';
+
+
+class Welcome extends React.Component {
+  render() {
+    return <h1>Hello, {this.props.name}</h1>;
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+    <Welcome name= {"crytodash1"} />
   );
+    }
 }
 
 export default App;
