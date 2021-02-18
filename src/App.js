427f5a2ee50b9f1ae8d815eb8eddee45e050d9fe
daccatime <dawsonmcnally@outlook.com>
2021-02-18 16:16:33 +0000

    test VSC

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 
 import React, {Component} from 'react';
+import WelcomeMessage from './WelcomeMessage' 
 
 
-class Welcome extends React.Component {
-  render() {
-    return <h1>Hello, {this.props.name}</h1>;
-  }
-}
 
 class App extends Component {
   render() {
     return (
-    <Welcome name= {"crytodash1"} />
+    <WelcomeMessage/>
   );
     }
 }
